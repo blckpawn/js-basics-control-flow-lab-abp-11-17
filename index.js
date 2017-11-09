34c489aeb2ac267e7c30c9aff64876b4c91ac6eb
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 15:08:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function scuberGreetingForFeet (start, end){
  const distance = (start - end) * 264;
 
   if (distance < 400){
-    return "This one is one me!";
+    return "This one is on me!";
   }
 }
