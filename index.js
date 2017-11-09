547e8e769db45ffd93f05eb37d2b8a2db7ef19e9
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 15:26:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet (start,end){
- const distance = Math.abs(start - end) * 264;
+ const distance = Math.abs(start - end) ;
 
   if (distance < 400){
     return "This one is on me!";
