f9fc4c286c7694d9a099df74bbc2fc8dddbadee3
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 15:11:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-function scuberGreetingForFeet (start, end){
+function scuberGreetingForFeet (end){
  const distance = Math.abs(start - end) * 264;
 
   if (distance < 400){
