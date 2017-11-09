b0922bcc75ad67761e752edf83024de0eed2ee59
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 15:52:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ function scuberGreetingForFeet (start){
   }else{
     if (start > 2000){
       return 'I will gladly take your thirty bucks.';
-    }else {
-      if (start > 2500){
-        return 'No can do.';
-      }
     }
   }
 
